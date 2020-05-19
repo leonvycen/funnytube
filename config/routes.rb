9a39e6f1b7b78cceb378fc918b4e15ad61ff9503
khoatmd <khoatmd@zigexn.vn>
2020-05-19 16:40:26 +0700

    setup session

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'main#home'
+  resources :users, only: %i[new create]
+  resources :videos, only: %i[new create]
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  get 'signup', to: 'users#new'
+  root to: 'main#home'
 end
